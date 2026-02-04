4a3d4ef73efda6131d58ecbfc196310af98ba741
yuku6665 <deemalowaidh@gmail.com>
2026-02-04 20:52:01 +0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+
+
+int cake=6;
+
+double c=66.6;
+
+cout<<"the cake is "<<cake<<endl;
+
+cout<<"and the c is "<<c<<endl;
+
+return 0; }
